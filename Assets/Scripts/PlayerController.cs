fbc50b7954c70fd7fca45c4573fb6ac80268d168
Jan Ong <JETZero3@gmail.com>
2016-04-16 17:01:49 -0600

    Player Movement
    Added basic player movement using a C# script and Unity libraries.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+    // Variable to control the speed of the object
+    public float speed;
+
+    // Variable for the rigid body component of the player
+    private Rigidbody2D ufo;
+
+    void Start()
+    {
+        // Get a reference to the rigid body component, and store in a variable. 
+        ufo = GetComponent<Rigidbody2D> ();
+    }
+
+    void FixedUpdate()
+    {
+        // Store horizontal input
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        // Store vertical input
+        float moveVertical = Input.GetAxis("Vertical");
+
+        // Make a Vector2 variable based on the inputs from the keyboard
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        // Make a force of the player input multiplied by the speed set. 
+        ufo.AddForce(movement * speed);
+    }
+}
